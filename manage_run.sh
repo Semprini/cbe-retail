db1a7d2e05b03c5b6b0af4be35cfcc77c0c789fc
Paul Atkin <paul@sphinx.co.nz>
2017-02-23 14:35:18 +1300

    Added specific app migrations to run script

--- manage_run.sh
+++ manage_run.sh
@@ -14,6 +14,7 @@ DATABASES = {\n\
     }\n\
 }\n" > /code/retail/local_settings.py
 
+python manage.py makemigrations business_interaction customer location trouble physical_object supplier_partner resource human_resources product pricing sale
 python manage.py migrate
 #uwsgi --ini uwsgi.ini
 python manage.py runserver 0.0.0.0:8000
