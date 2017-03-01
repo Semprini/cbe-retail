085a6d2f27c8f6f91cc1094585df56237c1708dc
Paul Atkin <paul@sphinx.co.nz>
2017-03-01 13:41:35 +1300

    Added docker compose

--- manage_run.sh
+++ manage_run.sh
@@ -17,5 +17,5 @@ DATABASES = {\n\
 #python manage.py migrate auth contenttypes
 #python manage.py makemigrations business_interaction location physical_object resource customer trouble supplier_partner human_resources product pricing sale
 python manage.py migrate
-uwsgi --ini uwsgi.ini
-#python manage.py runserver 0.0.0.0:8000
+#uwsgi --ini uwsgi.ini
+python manage.py runserver 0.0.0.0:8000
