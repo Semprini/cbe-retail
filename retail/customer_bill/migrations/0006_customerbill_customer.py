96b85e8eb604768421635b77753a7379be4899f9
Semprini <atkinpa@gmail.com>
2017-08-28 16:36:19 +1200

    Changed default on migration

--- retail/customer_bill/migrations/0006_customerbill_customer.py
+++ retail/customer_bill/migrations/0006_customerbill_customer.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='customerbill',
             name='customer',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='customer.Customer'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='customer.Customer'),
             preserve_default=False,
         ),
     ]
