ccc7ae8ca0e216be7b5c04117d9aaf8c2d1ae50a
Semprini <atkinpa@gmail.com>
2017-09-08 14:10:40 +1200

    Fixed loyalty worker

--- microservices/market_to_customers/calculate_loyalty_transaction/calculate_loyalty_transaction.py
+++ microservices/market_to_customers/calculate_loyalty_transaction/calculate_loyalty_transaction.py
@@ -27,32 +27,32 @@ class CalculateLoyaltyTransaction(QueueTriggerPattern):
     def worker(self, message_json):
         print( "Should override worker and do something here" )
 
-    # If the customer swiped a card then add some airpoints
-    if message_json['identification'] != None:
-        logging.info( "Sale with Airpoints triggered loyalty calc: {0}".format(message_json['identification']) )
-        total=float(message_json['total_amount'])
-        loyalty_amount = total * LOYALTY_RATE
-        
-        # Fill out LoyaltyTransaction json
-        data = loyalty_transaction_template\
-            .replace("{SALE}","{}".format(message_json['url']))\
-            .replace('{AMOUNT}',"{:0.2f}".format(loyalty_amount))\
-            .replace('{ID}',"{}".format(message_json['identification']))
+        # If the customer swiped a card then add some airpoints
+        if message_json['identification'] != None:
+            logging.info( "Sale with Airpoints triggered loyalty calc: {0}".format(message_json['identification']) )
+            total=float(message_json['total_amount'])
+            loyalty_amount = total * LOYALTY_RATE
             
-        # Create a new LoyaltyTransaction transaction by calling API
-        headers = {'Content-type': 'application/json',}
-        response = requests.post(LOYALTY_TRANSACTION_URL, data=data, headers=headers, auth=(API_USER, API_PASS))
-        if response.status_code == 201: # (201) Created
-            logging.info( "Loyalty transaction created" )
-        elif response.status_code >= 500 or response.status_code in (401,403):
-            logging.warning( "Retryable error creating loyalty transaction" )
-            logging.info( response.__dict__ )
-            raise RequeableError()
-        else:   
-            # Fatal errors which can't be retried
-            logging.error( "Fatal error creating loyalty transaction" )
-            logging.info( response.__dict__ )
-            raise FatalError()
+            # Fill out LoyaltyTransaction json
+            data = loyalty_transaction_template\
+                .replace("{SALE}","{}".format(message_json['url']))\
+                .replace('{AMOUNT}',"{:0.2f}".format(loyalty_amount))\
+                .replace('{ID}',"{}".format(message_json['identification']))
+                
+            # Create a new LoyaltyTransaction transaction by calling API
+            headers = {'Content-type': 'application/json',}
+            response = requests.post(LOYALTY_TRANSACTION_URL, data=data, headers=headers, auth=(API_USER, API_PASS))
+            if response.status_code == 201: # (201) Created
+                logging.info( "Loyalty transaction created" )
+            elif response.status_code >= 500 or response.status_code in (401,403):
+                logging.warning( "Retryable error creating loyalty transaction" )
+                logging.info( response.__dict__ )
+                raise RequeableError()
+            else:   
+                # Fatal errors which can't be retried
+                logging.error( "Fatal error creating loyalty transaction" )
+                logging.info( response.__dict__ )
+                raise FatalError()
 
         
 if __name__ == "__main__":
