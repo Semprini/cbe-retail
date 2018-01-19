7c1d0ed05cd7f2d9f718f716dbdc8de5ef11eac7
Paul Atkin <paul@sphinx.co.nz>
2018-01-19 15:31:44 +1300

    Removed tests for utils

--- retail/sale/tests.py
+++ retail/sale/tests.py
@@ -1,27 +1,2 @@
 from django.test import TestCase
 
-from retail.sale.models import Sale
-from retail.sale.utils import ImportDSR, ImportDSRLine, dsr_sample, dsr_sale
-
-class SaleTestCase(TestCase):
-
-    def test_bulk_dsr(self):
-        """
-        Make sure bulk imports of DSR files work
-        """
-        ImportDSR( dsr_sample, "X1" )
-        self.assertTrue(len(Sale.objects.all())>0)
-        
-    def test_single_dsr(self):
-        """
-        Make sure bulk imports of DSR files work
-        """
-        sale = ImportDSRLine(dsr_sale)
-        self.assertTrue(len(Sale.objects.all())>0)
-
-    def test_curl(self):
-        """
-        curl -X POST http://127.0.0.1/api/sale/sale/create_from_dsr/ -u <uname>:<pw> -H "Content-Type: text/plain" --data "@<filename>"
-        """
-        self.assertTrue(True)
-        
