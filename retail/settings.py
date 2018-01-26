072b1230c7728995037d90ab5e16c0053af73c7e
semprini <atkinpa@gmail.com>
2018-01-26 01:02:33 +0000

    moved static root to project parent

--- retail/settings.py
+++ retail/settings.py
@@ -192,7 +192,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/usr/local/apache2/htdocs/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 
 # LimitDepthMixin serializer setting
 DEPTH_MAX = 2
