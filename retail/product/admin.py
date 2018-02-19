a605b4f23ac3ee0f017bff610bd9daf2b27e4d03
Paul Atkin <paul@sphinx.co.nz>
2018-02-19 13:54:06 +1300

    Updated prod category admin

--- retail/product/admin.py
+++ retail/product/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 
 from retail.product.models import Product, ProductOffering, ProductCategory, ProductStock, ProductStockTake
 
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ( 'code', 'level', 'name', 'description', )
+    list_filter = ( 'level', )
+
+
 admin.site.register(Product)
 admin.site.register(ProductOffering)
-admin.site.register(ProductCategory)
+admin.site.register(ProductCategory,ProductCategoryAdmin)
 admin.site.register(ProductStock)
 admin.site.register(ProductStockTake)
 
