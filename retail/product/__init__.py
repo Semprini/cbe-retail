a1725140c017737ba5d6c0c54e83895158a3e0a0
Paul Atkin <paul@sphinx.co.nz>
2018-02-19 17:13:27 +1300

    Added notification for product

---
+++ retail/product/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'retail.product.apps.ProductConfig'
--- retail/product/apps.py
+++ retail/product/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_save, post_delete
+from django.conf import settings
 
 class ProductConfig(AppConfig):
-    name = 'product'
+    name = 'retail.product'
+
+    def ready(self):
+        import cbe.signals
+        from retail.product.models import Product
+        from retail.product.serializers import ProductSerializer
+
+        exchange_prefix = settings.MQ_FRAMEWORK['EXCHANGE_PREFIX'] + self.name
+        exchange_header_list = ('status',)
+        
+        post_save.connect(  cbe.signals.notify_extra_args(   serializer=ProductSerializer, 
+                                                                exchange_prefix=exchange_prefix + ".Product",
+                                                                exchange_header_list=exchange_header_list)(cbe.signals.notify_save_instance), 
+                            sender=Product, weak=False)    
