1f255e837a37f5b41b61fd8a5180497458e511a1
Paul Atkin <paul@sphinx.co.nz>
2018-02-19 16:32:38 +1300

    Updated prod  admin

--- retail/product/admin.py
+++ retail/product/admin.py
@@ -6,8 +6,11 @@ class ProductCategoryAdmin(admin.ModelAdmin):
     list_display = ( 'code', 'level', 'name', 'description', )
     list_filter = ( 'level', )
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ( 'code', 'status', 'name', 'brand', 'sub_brand', 'tax_code', 'impulse_item', 'key_value_item', 'core_abc', 'core_range', 'range', 'dangerous_classification', 'relative_importance_index', 'exclusive' )
+    list_filter = ( 'status','impulse_item', 'key_value_item', 'core_abc', 'range' )
 
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductOffering)
 admin.site.register(ProductCategory,ProductCategoryAdmin)
 admin.site.register(ProductStock)
