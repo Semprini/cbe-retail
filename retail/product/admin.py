a896865500934415ef85509d7b2389bbace67ae7
Paul Atkin <paul@sphinx.co.nz>
2018-02-23 17:30:22 +1300

    Added filter for department

--- retail/product/admin.py
+++ retail/product/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from retail.product.models import Product, ProductOffering, ProductCategory, ProductStock, ProductStockTake, SupplierProduct
 
+class DepartmentListFilter(admin.SimpleListFilter):
+    title = 'department'
+    parameter_name = 'department'
+    def lookups(self, request, model_admin):
+        result = ProductCategory.objects.filter(level='department')
+        return [(str(x.id), x) for x in result]
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            return queryset.filter(department__id=self.value())
+        else:
+            return queryset
+
 class ProductCategoryAdmin(admin.ModelAdmin):
     list_display = ( 'code', 'level', 'name', 'description', )
     list_filter = ( 'level', )
@@ -15,7 +28,7 @@ class ProductAdmin(admin.ModelAdmin):
     
 class ProductOfferingAdmin(admin.ModelAdmin):
     list_display = ( 'sku', 'product', 'department', 'sub_department', 'fineline', 'unit_of_measure', 'retail_price', 'average_cost_price', )
-    list_filter = ( 'unit_of_measure','department',  )
+    list_filter = ( 'unit_of_measure',DepartmentListFilter,  )
     raw_id_fields = ('product',)
     search_fields = ['sku','product']
 
